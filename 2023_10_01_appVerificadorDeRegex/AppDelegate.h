3e702bfa54ded98e9433fef881c5447827d8e8db
IvanNunez25 <ivan25606@gmail.com>
2023-10-01 13:00:19 -0600

    Initial Commit

---
+++ 2023_10_01_appVerificadorDeRegex/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  2023_10_01_appVerificadorDeRegex
+//
+//  Created by Ivanovicx Nuñez on 01/10/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ 2023_10_01_appVerificadorDeRegex/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  2023_10_01_appVerificadorDeRegex
+//
+//  Created by Ivanovicx Nuñez on 01/10/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
